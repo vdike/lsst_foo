78ffec0298b20abf30dcfbc3ad5ce7b9afb0214b
Nathanial Hendler <nathanhendler@gmail.com>
2018-11-06 13:32:33 -0700

    This is Nathan's super function

---
+++ code.py
@@ -0,0 +1,6 @@
+
+def do_something(something):
+    """
+    Prints the argument passed to do_something
+    """
+    print(something)# this will make it much easier in future problems to see that something is actually happening
